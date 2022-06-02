0534faef8e2021459715650b172f135c34479ec7
TomeCarvalho <tomecarvalho@ua.pt>
2022-06-02 13:15:52 +0100

    TQSG11-19 DeliveriesDataGrid for home page

--- deliverize/src/components/DeliveriesDataGrid.jsx
+++ deliverize/src/components/DeliveriesDataGrid.jsx
@@ -6,34 +6,43 @@ const DeliveriesDataGrid = ({deliveries}) => {
     const [pageSize, setPageSize] = useState(10);
 
     const columns = [
-        {field: "buyer", headerName: "Buyer", type: "number", flex: 1},
-        {field: "rider", headerName: "Rider", flex: 1},
+        {
+            field: "company", headerName: "Company", flex: 1,
+            valueGetter: (params) => params.row.company.name
+        },
+        {field: "buyer", headerName: "Buyer", flex: 1},
+        {
+            field: "rider", headerName: "Rider", flex: 1,
+            valueGetter: (params) => params.row.rider?.name
+        },
         {field: "deliveryStatus", headerName: "Status", flex: 1},
         {
             field: "requestedAt",
             headerName: "Ordered",
             type: "dateTime",
-            flex: 1,
+            flex: 2,
             valueGetter: ({value}) => value && new Date(value)
         },
         {
             field: "acceptedAt",
             headerName: "Accepted",
             type: "dateTime",
-            flex: 1,
+            flex: 2,
             valueGetter: ({value}) => value && new Date(value)
         },
         {
             field: "deliveredAt",
             headerName: "Delivered",
             type: "dateTime",
-            flex: 1,
+            flex: 2,
             valueGetter: ({value}) => value && new Date(value)
         },
         {field: "origin", headerName: "Origin", flex: 1},
         {field: "destination", headerName: "Destination", flex: 1},
-        {field: "id", headerName: "ID", flex: 1},
-        {field: "price", headerName: "Price", flex: 1},
+        {field: "id", headerName: "ID", flex: .5},
+        {field: "price", headerName: "Price", flex: .5,
+            valueGetter: (params) => `${params.row.price}€`
+        },
     ];
 
     return (
